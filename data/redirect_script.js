f0d4c34eb730233b013c75c9126711e7ba2c3a8c
dyeray <dyeray@gmail.com>
2014-12-25 21:31:12 +0000

    Now domains to redirect can be configured in about:config. Added all known blogpost domains for now. Remaded regex, and made more simple the data script (NoScript compatible mode)

--- data/redirect_script.js
+++ data/redirect_script.js
@@ -1,20 +1,3 @@
-const BLOGSPOT_COM_REGEXP = /(https?:\/\/.*\.blogspot\.com)\.[^/]*(\/.*?)$/g;
-const BLOGSPOT_OTHER_REGEXP = /(https?:\/\/.*\.blogspot)\.it(\/.*?)$/g;
-
-function get_redirect(url) {
-    var com = BLOGSPOT_COM_REGEXP.exec(url)
-    var other = BLOGSPOT_OTHER_REGEXP.exec(url)
-    // Check for blogspot.com.* domains
-    if (com != null && com[0]) {
-        return com[1] + "/ncr" + com[2];
-    }
-    // Check for blogspot.x domains (currently x = it)
-    else if (other != null && other[0]) {
-        return other[1] + ".com/ncr" + other[2];
-    }
-    return null;
-}
-redirect = get_redirect(document.URL);
-if (redirect) {
-    window.location = redirect;
-}
+var regexp = /(https?:\/\/.*\.blogspot\.)[^\/]*(\/.*)?$/;
+match = regexp.exec(document.URL);
+window.location = match[1] + "com/ncr" + match[2] || "";
