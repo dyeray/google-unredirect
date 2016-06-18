8de1918be1191486c742dec6c6e081adbb5ddfda
dyeray <dyeray@gmail.com>
2016-06-18 16:56:54 +0200

    Update indentation (2 spaces)

--- index.js
+++ index.js
@@ -8,45 +8,45 @@ AC_DOMAINS = "extensions.google-unredirect@dyeray.domains";
 
 
 function onDomainsUpdated() {
-    var redirected_domains = generateDomainLists();
-    registerRedirectFunction(redirected_domains);
+  var redirected_domains = generateDomainLists();
+  registerRedirectFunction(redirected_domains);
 }
 
 
 function generateDomainLists() {
-    var domains = preferences.get(AC_DOMAINS, DEFAULT_DOMAINS).split(";");
-    var redirected_domains = [];
-    for (var i = 0; i < domains.length; i++) {
-        redirected_domains.push("*.blogspot." + domains[i]);
-    }
-    return redirected_domains;
+  var domains = preferences.get(AC_DOMAINS, DEFAULT_DOMAINS).split(";");
+  var redirected_domains = [];
+  for (var i = 0; i < domains.length; i++) {
+    redirected_domains.push("*.blogspot." + domains[i]);
+  }
+  return redirected_domains;
 }
 
 function setAboutConfig() {
-    // Set default about:config value if it doesn't exist
-    if (!preferences.has(AC_DOMAINS)) {
-        preferences.set(AC_DOMAINS, DEFAULT_DOMAINS);
-    }
-    simple_prefs.on("domains", onDomainsUpdated);
+  // Set default about:config value if it doesn't exist
+  if (!preferences.has(AC_DOMAINS)) {
+    preferences.set(AC_DOMAINS, DEFAULT_DOMAINS);
+  }
+  simple_prefs.on("domains", onDomainsUpdated);
 }
 
 function registerRedirectFunction(redirected_domains) {
-    if (typeof pm !== 'undefined') {
-      pm.destroy();
-    }
-    pm = pageMod.PageMod({
-        contentScriptWhen: 'start',
-        include: redirected_domains,
-        contentScriptFile: data.url("redirect_script.js")
-    });
+  if (typeof pm !== 'undefined') {
+    pm.destroy();
+  }
+  pm = pageMod.PageMod({
+    contentScriptWhen: 'start',
+    include: redirected_domains,
+    contentScriptFile: data.url("redirect_script.js")
+  });
 }
 
 exports.main = function() {
-    setAboutConfig(); // Create settings in about config and configure listener in case settings are changed.
-    var redirected_domains = generateDomainLists();    // Generate in ram the lists of domains to redirect from the settings.
-    registerRedirectFunction(redirected_domains);   // Register the function that is going to do the redirect (it depends on what method has been selected).
+  setAboutConfig(); // Create settings in about config and configure listener in case settings are changed.
+  var redirected_domains = generateDomainLists();    // Generate in ram the lists of domains to redirect from the settings.
+  registerRedirectFunction(redirected_domains);   // Register the function that is going to do the redirect (it depends on what method has been selected).
 };
 
 exports.onUnload = function() {
-    simple_prefs.removeListener("domains", onDomainsUpdated);
+  simple_prefs.removeListener("domains", onDomainsUpdated);
 };
